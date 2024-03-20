893b802d0457b2679c8563bf63a21b864dc0956b
Gözde Alıcı <gozdeealici@gmail.com>
2024-03-20 12:57:21 +0300

    variable

---
+++ JavaScript/template.js
@@ -0,0 +1,61 @@
+// Comment
+
+/*
+Multi Comment
+Multi Comment
+*/
+
+// console.log("Merhabalar")
+
+// window.console.log("Window Merhabalar")
+
+// console.warn("Console Warn")
+
+// console.info("Console Info")
+
+// console.error("Console.Error")
+
+// alert("Alert")
+
+// VARIABLE
+
+// VAR DEGİŞKENİ BİRDEN FAZLA KULLANILABİLİR
+
+var companyName = "Miuul";
+console.log(companyName);
+
+
+var companyName44 = "Miuul";
+console.log(companyName44);
+
+
+var _companyName = "Miuul";
+console.log(_companyName);
+
+
+var $_companyName = "Miuul";
+console.log($_companyName);
+
+// Yazılmaz, türkçe karakter de kullanamayız.
+
+// var 30company = "Miuul";
+// console.log(30company);
+
+// LET
+
+let companyName2 = "Miuul";
+companyName2 = "Miuul Frontend";
+console.warn(companyName2);
+
+// kullanamayız tekrar
+// let companyName2 = "Miuul";
+// console.warn(companyName2);
+
+// CONST
+
+const companyName3 = "Miuul Frontend JS";
+console.warn(companyName3);
+
+// kullanamayız,değiştirilemez.
+// const companyName3 = "Miuul Frontend JS HTML";
+// console.warn(companyName3);
